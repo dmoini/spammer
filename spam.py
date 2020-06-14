a80c96f226cf8b3c72ed89685642966a6e354465
Donovan Moini <donovanmoini@gmail.com>
2020-06-14 03:51:54 -0700

    Options automatically update when script added

--- spam.py
+++ spam.py
@@ -1,27 +1,31 @@
 import pyautogui
 import inquirer
 import time
+import os
 
+SCRIPTS_DIR = './scripts/'
 COUNTDOWN_SECONDS = 10
 
 
-def chooseTextFile():
+def chooseFile():
+    availableTexts = [os.path.splitext(' '.join(f.capitalize() for f in file.split('-')))[0]
+                      for file in os.listdir(SCRIPTS_DIR)]
     questions = [
         inquirer.List('textFile',
-                      message='What text file would you like to spam?',
-                      choices=['Bee Movie'],
+                      message='What text file would you like to use?',
+                      choices=availableTexts,
                       ),
     ]
     answers = inquirer.prompt(questions)
-    return answers['textFile'].replace(' ', '-').lower()
+    return SCRIPTS_DIR + answers['textFile'].replace(' ', '-').lower() + '.txt'
 
 
 def main():
-    filePath = './scripts/' + chooseTextFile() + '.txt'
+    filePath = chooseFile()
     file = open(filePath, 'r')
     print(f'The spamming will begin in {COUNTDOWN_SECONDS} second(s)')
     for i in range(COUNTDOWN_SECONDS, 0, -1):
-        print(f'{i}')
+        print(i)
         time.sleep(1)
     for line in file:
         for word in line.split():
